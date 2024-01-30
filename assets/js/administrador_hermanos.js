b0de75abd19460769d50aaec75bf89e919a3849b
Pau Muñoz Olivares <pau.mu.ol@gmail.com>
2024-01-30 23:59:59 +0100

    .

--- assets/js/administrador_hermanos.js
+++ assets/js/administrador_hermanos.js
@@ -16,16 +16,46 @@
 //     document.querySelector('tbody').innerHTML = htmlTableCode
 // })();
 
-document.querySelector('table').addEventListener('click', e => {
-    let target = e.target;
-    console.log("🚀 ~ document.querySelector ~ target:", target)
+document.querySelector('table')
+    .addEventListener('click', e => switch_ClickController(e.target))
 
-    if (target.closest('.options-parent')) {
-        const optionParent = target.closest('.options-parent')
-        optionParent.querySelector('.options').classList.add('show');
-    } else {
-        document.querySelectorAll('.options-parent>.options').forEach(e => e.classList.remove('show'))
+const switch_ClickController = (target) => {
+
+    // si el click esta dentro de una table_row
+    if (target.closest('tr')) {
+        console.log("🚀 ~ target:", target)
+        let table_row = target.closest('tr');
+        let table_row_active = document.querySelector('table tr.active');
+        let btnOptionsClicked = target.classList.contains('btn-options');
+
+        // si se selecciona el table header, out
+        if (table_row.querySelector('th')) return;
+
+        // si existe una table_row activa, desactivarla
+        if (table_row_active) {
+            table_row_active.classList.remove('active');
+            table_row_active.querySelector('input[type="checkbox"]').checked = false;
+            if (table_row == table_row_active) return;
+        }
+
+        // if (document.querySelector('.options-parent>.options.show')) {
+        //     document.querySelector('.options-parent>.options.show').classList.remove('show')
+        // }
+
+        // // si se clica al btn de opciones
+        // if (btnOptionsClicked) {
+        //     const optionParent = target.closest('.options-parent')
+        //     optionParent.querySelector('.options').classList.add('show');
+        // }
+
+        // activar la fila clicada
+        table_row.classList.add('active');
+        table_row.querySelector('input[type="checkbox"]').checked = true
     }
+}
 
-})
+
+
+
+
 
